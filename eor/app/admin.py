901c28c25439a82296fdc2771d17bed0d214a452
karasu <gustaucastells@gmail.com>
2024-03-18 12:06:08 +0100

    Adding some data to do initial tests

--- eor/app/admin.py
+++ eor/app/admin.py
@@ -1,3 +1,28 @@
+""" register models for admin module """
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question
+from .models import Choice
+from .models import Factors
+from .models import Team
+from .models import Competition
+from .models import Match
+from .models import Set
+from .models import MatchSetsFactors
+from .models import ExchangeInfo
+from .models import Exchange
+from .models import PlayerFactors
+
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Factors)
+admin.site.register(Team)
+admin.site.register(Competition)
+admin.site.register(Match)
+admin.site.register(Set)
+admin.site.register(MatchSetsFactors)
+admin.site.register(ExchangeInfo)
+admin.site.register(Exchange)
+admin.site.register(PlayerFactors)
--- eor/app/models.py
+++ eor/app/models.py
@@ -4,20 +4,10 @@ from django.db import models
 from django.utils.timezone import now
 from django.contrib.auth.models import User
 
-class Question(models.Model):
-    """ Stores a poll question """
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-
-class Choice(models.Model):
-    """ Stores a question answer (choice) """
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-class Factors(models.Model):
+class PlayerStatus(models.Model):
     """ Stores player status """
-    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    player = models.ForeignKey(
+        User, on_delete=models.CASCADE)
     motivated = models.BooleanField()
     activated = models.BooleanField()
     concentrated = models.BooleanField()
@@ -36,39 +26,45 @@ class Competition(models.Model):
 
 class Match(models.Model):
     """ Generic match info """
-    competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
-    home = models.ForeignKey(Team, on_delete=models.CASCADE)
-    visitant = models.ForeignKey(Team, on_delete=models.CASCADE)
+    competition = models.ForeignKey(
+        Competition, on_delete=models.CASCADE)
+    home = models.ForeignKey(
+        Team, related_name='home_team', on_delete=models.CASCADE)
+    visitor = models.ForeignKey(
+        Team, related_name='visitor_team', on_delete=models.CASCADE)
     date = models.DateField()
     jornada = models.IntegerField()
 
 class Set(models.Model):
     """ Stores ended set info """
-    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    match = models.ForeignKey(
+        Match, on_delete=models.CASCADE)
     num_set = models.IntegerField()
     home_points = models.IntegerField()
     visitor_points = models.IntegerField()
 
-class MatchSetsFactors(models.Model):
+class SetPlayerStatus(models.Model):
     """ Store factors for each set """
     set = models.ForeignKey(Set, on_delete=models.CASCADE)
-    factors = models.ForeignKey(Factors, on_delete=models.CASCADE)
+    factors = models.ForeignKey(PlayerStatus, on_delete=models.CASCADE)
 
 class ExchangeInfo(models.Model):
     """ Info to store in each exchange of points (each row)"""
     team = models.ForeignKey(Match, on_delete=models.CASCADE)
     is_change = models.BooleanField()
-    is_time = models.BooleanField()
+    is_time_out = models.BooleanField()
     points = models.IntegerField()
 
 class Exchange(models.Model):
     """ Match row """
     time = models.DateTimeField(default=now)
     set = models.ForeignKey(Set, on_delete=models.CASCADE)
-    home_info = models.ForeignKey(ExchangeInfo, on_delete=models.CASCADE)
-    visitor_info = models.ForeignKey(ExchangeInfo, on_delete=models.CASCADE)
+    home_info = models.ForeignKey(
+        ExchangeInfo, related_name='home_exchange_info', on_delete=models.CASCADE)
+    visitor_info = models.ForeignKey(
+        ExchangeInfo, related_name='visitor_exchange_info', on_delete=models.CASCADE)
 
-class PlayerFactors(models.Model):
+class ExchangePlayerStatus(models.Model):
     """ Player factors in a point exchange """
     exchange = models.ForeignKey(Exchange, on_delete=models.CASCADE)
-    factors = models.ForeignKey(Factors, on_delete=models.CASCADE)
+    factors = models.ForeignKey(PlayerStatus, on_delete=models.CASCADE)
